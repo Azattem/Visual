5cb7af66547515d2b5970b6d1a735f4c2dc94bf4
azatt <azatteam2013@yandex.ru>
2022-07-07 19:01:40 +0500

    Azat

--- src/VisualDraw.java
+++ src/VisualDraw.java
@@ -9,17 +9,16 @@ public class VisualDraw {
     private double x0;
     private double y0;
     private double scale;
-    private double lenghtX0 = 1 * scale;
-    private double lenghtY0 = 1 * scale;
+
+
     VisualDraw( Canvas canvas){
     this.canvas = canvas;
-        System.out.println((int)canvas.getWidth());
     this.width = (int)canvas.getWidth();
     this.height = (int)canvas.getHeight();
     this.x0 = (width / 2);
     this.y0 = (height / 2);
-
     }
+
     public void UpdateScale(double Scale){
     scale = Math.min(height,width)/Scale;
     }
@@ -29,20 +28,19 @@ public class VisualDraw {
         Color[]colors256=colorArray();
         for (int i = 1; i < width - 1; i++) {
             for (int j = 1; j < height - 1; j++) {
-                double x = (i - x0) / lenghtX0;
-                double y = (j - y0) / lenghtY0;
-                System.out.println(x+" "+y);
+                double x = (i - x0) / (1*scale);
+                double y = (j - y0) / (1*scale);
                 z = new Complex(x, y);
                 System.out.println(z.x+" "+z.y);
                 int color = getColor(z);
-                //System.out.println(colors256[color]);
+                System.out.println(colors256[color]);
                 canvas.getGraphicsContext2D().getPixelWriter().setColor(i,j,colors256[color]);
             }
         }
     }
     public void SetDismensions(int x,int y){
-    this.x0 = (width / 2+x);
-    this.y0 = (height / 2+y);
+    this.x0 = (width / 2)+x;
+    this.y0 = (height / 2)+y;
     }
     private static Color[] colorArray() {
         int red[] = {0, 36, 73, 109, 146, 182, 219, 255};
