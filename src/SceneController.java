e6dc5d6001ddca2484fcd8663a69183354286d0d
Ainur <ainurforex@mail.ru>
2022-07-08 17:36:32 +0500

    ver

--- src/SceneController.java
+++ src/SceneController.java
@@ -7,84 +7,181 @@ public class SceneController {
     public TextField Scale;
     public TextField Scalex;
     public TextField Scaley;
+    public static double x0Mem;
+    public static double y0Mem;
+    public static int RGBPalitre = 2;
+    public static int approxBox = 0;
+    private static boolean noFirstRun=false;
     public javafx.scene.canvas.Canvas Canvas;
-    public void Update() {
-        VisualDraw draw=new VisualDraw(Canvas);
-        draw.SetDismensions(GetScaleX(),GetScaleY());
-        draw.UpdateScale(GetScale());
-        draw.Update();
+
+
+
+
+
+    public void update() {
+
+        VisualDraw draw = new VisualDraw(Canvas);
+       draw.updateScale(getScale());
+        draw.updateRGB(RGBPalitre);
+        if(noFirstRun){
+            draw.updateXY(approxBox,x0Mem,y0Mem);
+            approxBox=0;
+        }
+        noFirstRun=true;
+        draw.update();
+        x0Mem=draw.getX0();
+        y0Mem=draw.getY0();
     }
-    private void ChangeScale(int s) {
-    if(Integer.parseInt(Scale.getText())+s>=1) {
-        Scale.setText((Integer.parseInt(Scale.getText()) + s) + "");
+
+
+
+    private void changeScale(double s) {
+        double currentScale=Double.parseDouble(Scale.getText());
+        if (currentScale + (currentScale*s/100) >= 0) {
+            Scale.setText(currentScale + (currentScale*s/100) + "");
+            update();
+        }
     }
+
+    private void changeScaleX(int s) {
+        Scalex.setText((Integer.parseInt(Scalex.getText()) + s) + "");
     }
-    private void ChangeScaleX(int s) {
-            Scalex.setText((Integer.parseInt(Scalex.getText()) + s) + "");
+
+    private void changeScaleY(int s) {
+        Scaley.setText((Integer.parseInt(Scaley.getText()) + s) + "");
     }
-    private void ChangeScaleY(int s) {
-            Scaley.setText((Integer.parseInt(Scaley.getText()) + s) + "");
+
+    private double getScale() {
+        return Double.parseDouble(Scale.getText());
     }
-    private int GetScale(){
-    return Integer.parseInt(Scale.getText());
-    }
-    private int GetScaleX(){
+
+    private int getScaleX() {
         return Integer.parseInt(Scalex.getText());
     }
-    private int GetScaleY(){
+
+    private int getScaleY() {
         return Integer.parseInt(Scaley.getText());
     }
 
     @FXML
-    private void Increase(ActionEvent actionEvent) {
-        ChangeScale(1);
+    private void approx1(ActionEvent actionEvent) {
+        approxBox=1;
+        update();
+    }
+    @FXML
+    private void approx2(ActionEvent actionEvent) {
+        approxBox=2;
+        update();
+    }
+    @FXML
+    private void approx3(ActionEvent actionEvent) {
+        approxBox=3;
+        update();
+    }
+    @FXML
+    private void approx4(ActionEvent actionEvent) {
+        approxBox=4;
+        update();
+    }
+    @FXML
+    private void approx5(ActionEvent actionEvent) {
+        approxBox=5;
+        update();
+    }@FXML
+    private void approx6(ActionEvent actionEvent) {
+        approxBox=6;
+        update();
+    }@FXML
+    private void approx7(ActionEvent actionEvent) {
+        approxBox=7;
+        update();
+    }@FXML
+    private void approx8(ActionEvent actionEvent) {
+        approxBox=8;
+        update();
+    }@FXML
+    private void approx9(ActionEvent actionEvent) {
+        approxBox=9;
+        update();
+    }
+    @FXML
+    private void increase(ActionEvent actionEvent) {
+        changeScale(1);
     }
 
     @FXML
-    private void Decrease(ActionEvent actionEvent) {
-        ChangeScale(-1);
-    }
-    @FXML
-    private void Increase10(ActionEvent actionEvent) {
-        ChangeScale(+10);
-    }
-    @FXML
-    private void Decrease10(ActionEvent actionEvent) {
-        ChangeScale(-10);
-    }
-    @FXML
-    private void IncreaseX(ActionEvent actionEvent) {
-        ChangeScaleX(1);
+    private void decrease(ActionEvent actionEvent) {
+        changeScale(-1);
     }
 
     @FXML
-    private void DecreaseX(ActionEvent actionEvent) {
-        ChangeScaleX(-1);
-    }
-    @FXML
-    private void Increase10X(ActionEvent actionEvent) {
-        ChangeScaleX(+10);
-    }
-    @FXML
-    private void Decrease10X(ActionEvent actionEvent) {
-        ChangeScaleX(-10);
-    }
-    @FXML
-    private void IncreaseY(ActionEvent actionEvent) {
-        ChangeScaleY(1);
+    private void increase1(ActionEvent actionEvent) {
+        changeScale(+10);
     }
 
     @FXML
-    private void DecreaseY(ActionEvent actionEvent) {
-        ChangeScaleY(-1);
+    private void decrease1(ActionEvent actionEvent) {
+        changeScale(-10);
     }
+
     @FXML
-    private void Increase10Y(ActionEvent actionEvent) {
-        ChangeScaleY(+10);
+    private void increaseX(ActionEvent actionEvent) {
+        changeScaleX(1);
     }
+
     @FXML
-    private void Decrease10Y(ActionEvent actionEvent) {
-        ChangeScaleY(-10);
+    private void decreaseX(ActionEvent actionEvent) {
+        changeScaleX(-1);
+    }
+
+    @FXML
+    private void increase10X(ActionEvent actionEvent) {
+        changeScaleX(+10);
+    }
+
+    @FXML
+    private void decrease10X(ActionEvent actionEvent) {
+        changeScaleX(-10);
+    }
+
+    @FXML
+    private void increaseY(ActionEvent actionEvent) {
+        changeScaleY(1);
+    }
+
+    @FXML
+    private void decreaseY(ActionEvent actionEvent) {
+        changeScaleY(-1);
+    }
+
+    @FXML
+    private void increase10Y(ActionEvent actionEvent) {
+        changeScaleY(+10);
+    }
+
+    @FXML
+    private void decrease10Y(ActionEvent actionEvent) {
+        changeScaleY(-10);
+    }
+
+    @FXML
+    private void RGBChange(ActionEvent actionEvent) {
+        if (RGBPalitre < 7) {
+            RGBPalitre++;
+        }
+        if (RGBPalitre == 7) {
+            RGBPalitre = 1;
+        }
+        update();
+
+    }
+
+    @FXML
+    private void RGBRandom(ActionEvent actionEvent) {
+        RGBPalitre = 7;
+
+        update();
+
     }
 
 }
--- src/VisualDraw.java
+++ src/VisualDraw.java
@@ -1,6 +1,6 @@
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
-
+import javafx.scene.shape.Line;
 
 public class VisualDraw {
     private Canvas canvas;
@@ -10,62 +10,116 @@ public class VisualDraw {
     private double y0;
     private double scale;
 
+    private int paletteKoef;
+    private int paletteSize;
+    private int RGBPalitre;
 
-    VisualDraw( Canvas canvas){
-    this.canvas = canvas;
-    this.width = (int)canvas.getWidth();
-    this.height = (int)canvas.getHeight();
-    this.x0 = (width / 2);
-    this.y0 = (height / 2);
+    VisualDraw(Canvas canvas) {
+        this.canvas = canvas;
+        this.width = (int) canvas.getWidth();
+        this.height = (int) canvas.getHeight();
+        this.x0 = (width / 6) * 3;
+        this.y0 = (height / 6) * 3;
+        this.RGBPalitre = 2;
+        this.paletteKoef = 4;//min=2;max=256
+        this.paletteSize = paletteKoef * paletteKoef * (paletteKoef / 2);
     }
 
-    public void UpdateScale(double Scale){
-    scale = Math.min(height,width)/Scale;
+    public void updateScale(double Scale) {
+        scale = (Math.min(height, width) / 2) * Scale;
     }
 
-    public void Update(){
+    public void updateRGB(int UpdateRGB) {
+
+        RGBPalitre = UpdateRGB;
+    }
+
+    public void updateXY(int approxBox, double x0Mem, double y0Mem) {
+        int shiftX = 0;
+        int shiftY = 0;
+        boolean reset = false;
+        int shiftXdelta = width / 3;
+        int shiftYdelta = height / 3;
+        switch (approxBox) {
+            case (1):
+                shiftX = shiftXdelta;
+                shiftY = shiftYdelta;
+                break;
+            case (2):
+                shiftY = shiftYdelta;
+                break;
+            case (3):
+                shiftX = -shiftXdelta;
+                shiftY = shiftYdelta;
+                break;
+            case (4):
+                shiftX = shiftXdelta;
+                break;
+            case (5):
+                reset = true;
+                break;
+            case (6):
+                shiftX = -shiftXdelta;
+
+                break;
+            case (7):
+                shiftX = shiftXdelta;
+                shiftY = -shiftYdelta;
+                break;
+            case (8):
+                shiftY = -shiftYdelta;
+                break;
+            case (9):
+                shiftX = -shiftXdelta;
+                shiftY = -shiftYdelta;
+                break;
+        }
+
+        x0 = x0Mem + shiftX;
+        y0 = y0Mem + shiftY;
+        if (reset == true) {
+            reset = false;
+            x0 = (width / 6) * 3;
+            y0 = (height / 6) * 3;
+        }
+    }
+
+    public void update() {
         Complex z;
-        Color[]colors256=colorArray();
+        Color[] colorArray = ColorArray.colorArray(paletteKoef, RGBPalitre);
         for (int i = 1; i < width - 1; i++) {
             for (int j = 1; j < height - 1; j++) {
-                double x = (i - x0) / (1*scale);
-                double y = (j - y0) / (1*scale);
+                double x = (i - x0) / scale;
+                double y = (j - y0) / scale;
                 z = new Complex(x, y);
-                System.out.println(z.x+" "+z.y);
-                int color = getColor(z);
-                System.out.println(colors256[color]);
-                canvas.getGraphicsContext2D().getPixelWriter().setColor(i,j,colors256[color]);
-            }
-        }
-    }
-    public void SetDismensions(int x,int y){
-    this.x0 = (width / 2)+x;
-    this.y0 = (height / 2)+y;
-    }
-    private static Color[] colorArray() {
-        int red[] = {0, 36, 73, 109, 146, 182, 219, 255};
-        int green[] = red;
-        int blue[] = {0, 85, 170, 255};
-        Color colors256[] = new Color[256];
-        int m = 0;
 
-        for (int i = 0; i < 8; i++) {
+                int color = getColor(z, paletteSize);
+                canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, colorArray[color]);
 
-            for (int j = 0; j < 8; j++) {
 
-                for (int k = 0; k < 4; k++) {
-                    System.out.println(red[i]+" "+green[j]+" "+blue[k]);
-                    colors256[m++] = Color.rgb(red[i], green[j], blue[k]);
-
+                if (i == width / 3 || i == (width / 3) * 2) {
+                    canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.GRAY);
                 }
+                if (j == height / 3 || j == (height / 3) * 2) {
+                    canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.GRAY);
+                }
+
+
             }
         }
-        return colors256;
+
     }
-    private static int getColor(Complex z0) {
+
+    public void setDismensions(int x, int y) {
+        this.x0 = (width / 2) + x;
+        this.y0 = (height / 2) + y;
+    }
+
+
+    private static int getColor(Complex z0, int paletteSize) {
         Complex z = z0;
 
-        for (int i = 255; i > 1; i--) {
+        for (int i = paletteSize - 1; i > 1; i--) {
             if (z.abs() > 2) {
                 return i;
             }
@@ -75,4 +129,11 @@ public class VisualDraw {
         return 0;
     }
 
+    public double getX0() {
+        return x0;
+    }
+
+    public double getY0() {
+        return y0;
+    }
 }
