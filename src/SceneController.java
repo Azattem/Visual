f91c563e0915bc9b329281e9e432a9115f4200bc
Ainur <ainurforex@mail.ru>
2022-07-09 12:08:22 +0500

    Добавил приближение без смещения +100

--- src/SceneController.java
+++ src/SceneController.java
@@ -2,65 +2,93 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class SceneController {
     @FXML
     public TextField Scale;
-      private static double x0Mem;
+    private static double x0Mem;
     private static double y0Mem;
     private static double mouseX;
     private static double mouseY;
     private static int RGBPalitre = 2;
-    private static int approxBox = 0;
-    private static boolean noFirstRun=false;
+    private static boolean noFirstRun = false;
     public javafx.scene.canvas.Canvas Canvas;
-
 
 
     //Mouseevent позволяет получить кординаты мышки при нажатие оформляеться как написанно ниже, образаеясь к пременой e там есть методы для получения кардинат
     //public void update(MouseEvent e) {
-    public void update() {
+    public void updateScale() {
 
         VisualDraw draw = new VisualDraw(Canvas);
         draw.updateScale(getScale());
         draw.updateRGB(RGBPalitre);
 
-        if(noFirstRun){
-            mouseX=x0Mem;
-            mouseY=y0Mem;
-            draw.updateMouseXYStay(mouseX,mouseY,x0Mem,y0Mem);
-           approxBox=0;
+        if (noFirstRun) {
+            mouseX = x0Mem;
+            mouseY = y0Mem;
+            draw.updateMouseXYScale(mouseX, mouseY, x0Mem, y0Mem);
+
+        } else {
+            draw.setColorArray(ColorArray.colorArray(draw.getPaletteKoef(), RGBPalitre));
         }
-        noFirstRun=true;
+        noFirstRun = true;
         draw.update();
-        x0Mem=draw.getX0();
-        y0Mem=draw.getY0();
+        x0Mem = draw.getX0();
+        y0Mem = draw.getY0();
 
     }
+
     public void updateMouse(MouseEvent e) {
 
         VisualDraw draw = new VisualDraw(Canvas);
         draw.updateScale(getScale());
         draw.updateRGB(RGBPalitre);
 
-        if(noFirstRun){
-             mouseX=e.getSceneX();
-             mouseY=e.getSceneY();
-            draw.updateMouseXY(mouseX,mouseY,x0Mem,y0Mem);
+        if (noFirstRun) {
+            mouseX = e.getSceneX();
+            mouseY = e.getSceneY();
+            draw.updateMouseXY(mouseX, mouseY, x0Mem, y0Mem);
+        } else {
+            draw.setColorArray(ColorArray.colorArray(draw.getPaletteKoef(), RGBPalitre));
         }
-        noFirstRun=true;
+
+        noFirstRun = true;
         draw.update();
-        x0Mem=draw.getX0();
-        y0Mem=draw.getY0();
+        x0Mem = draw.getX0();
+        y0Mem = draw.getY0();
+
+    }
+
+    public void updateRGB() {
+
+        VisualDraw draw = new VisualDraw(Canvas);
+        draw.updateScale(getScale());
+        draw.setColorArray(ColorArray.colorArray(draw.getPaletteKoef(), RGBPalitre));
+        draw.updateRGB(RGBPalitre);
+
+        if (noFirstRun) {
+            mouseX = x0Mem;
+            mouseY = y0Mem;
+            draw.updateMouseXYStay(mouseX, mouseY, x0Mem, y0Mem);
+
+
+        }
+
+
+        noFirstRun = true;
+        draw.update();
+        x0Mem = draw.getX0();
+        y0Mem = draw.getY0();
 
     }
 
 
     private void changeScale(double s) {
-        double currentScale=Double.parseDouble(Scale.getText());
-        if (currentScale + (currentScale*s/100) >= 0) {
-            Scale.setText(currentScale + (currentScale*s/100) + "");
-            update();
+        double currentScale = Double.parseDouble(Scale.getText());
+        if (currentScale + (currentScale * s / 100) >= 0) {
+            Scale.setText(currentScale + (currentScale * s / 100) + "");
+            updateScale();
         }
     }
 
@@ -68,8 +96,6 @@ public class SceneController {
     private double getScale() {
         return Double.parseDouble(Scale.getText());
     }
-
-
 
 
     @FXML
@@ -83,13 +109,13 @@ public class SceneController {
     }
 
     @FXML
-    private void increase10(ActionEvent actionEvent) {
-        changeScale(+10);
+    private void increase100(ActionEvent actionEvent) {
+        changeScale(+100);
     }
 
     @FXML
-    private void decrease10(ActionEvent actionEvent) {
-        changeScale(-10);
+    private void decrease100(ActionEvent actionEvent) {
+        changeScale(-100);
     }
 
     @FXML
@@ -100,7 +126,7 @@ public class SceneController {
         if (RGBPalitre == 7) {
             RGBPalitre = 1;
         }
-        update();
+        updateRGB();
 
     }
 
@@ -108,7 +134,7 @@ public class SceneController {
     private void RGBRandom(ActionEvent actionEvent) {
         RGBPalitre = 7;
 
-        update();
+        updateRGB();
 
     }
 
--- src/VisualDraw.java
+++ src/VisualDraw.java
@@ -11,10 +11,13 @@ public class VisualDraw {
     private double scale;
 
     private int shiftKoef;
+    private static double xShift=0;
+    private static double yShift=0;
 
     private int paletteKoef;
     private int paletteSize;
     private int RGBPalitre;
+    private static Color[] colorArray;
 
     VisualDraw(Canvas canvas) {
         this.canvas = canvas;
@@ -23,13 +26,23 @@ public class VisualDraw {
         this.x0 = (width / 6) * 3;
         this.y0 = (height / 6) * 3;
         this.RGBPalitre = 2;
-        this.paletteKoef = 8;//min=2;max=256
+        this.paletteKoef = 16;//min=2;max=256
         this.paletteSize = paletteKoef * paletteKoef * (paletteKoef / 2);
         this.shiftKoef=3;
+
     }
+
+    public void setColorArray(Color[] colorArray) {
+        this.colorArray = colorArray;
+    }
+
+    public int getPaletteKoef() {
+        return paletteKoef;
+    }
+
     public void update() {
         Complex z;
-        Color[] colorArray = ColorArray.colorArray(paletteKoef, RGBPalitre);
+        //Color[] colorArray = ColorArray.colorArray(paletteKoef, RGBPalitre);
         for (int i = 1; i < width - 1; i++) {
             for (int j = 1; j < height - 1; j++) {
                 double x = (i - x0) / scale;
@@ -38,17 +51,19 @@ public class VisualDraw {
 
                 int color = getColor(z, paletteSize);
 
-                    canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, colorArray[color]);
+
+                canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, colorArray[color]);
 
 
 
+/*
                 if (i == width / 3 || i == (width / 3) * 2) {
                     canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.GRAY);
                 }
                 if (j == height / 3 || j == (height / 3) * 2) {
                     canvas.getGraphicsContext2D().getPixelWriter().setColor(i, j, Color.GRAY);
                 }
-
+*/
 
             }
         }
@@ -65,17 +80,27 @@ public class VisualDraw {
 
 
 
+    public void updateMouseXYScale(double mouseX, double mouseY, double x0Mem, double y0Mem) {
+        System.out.println("stay "+xShift + " " + yShift+" m "+mouseX+" "+mouseY);
+        updateMouseXY(width/2,height/2,x0Mem,y0Mem);
+        x0 = x0Mem+(xShift/100)*100-width/2;
+        y0 = y0Mem+(yShift/100)*100-height/2;
+
+
+    }
+
     public void updateMouseXYStay(double mouseX, double mouseY, double x0Mem, double y0Mem) {
 
         x0 = x0Mem;
         y0 = y0Mem;
 
-
     }
     public void updateMouseXY(double mouseX, double mouseY, double x0Mem, double y0Mem) {
 
-        x0 = x0Mem+(width/2-mouseX);
-        y0 = y0Mem+(height/2-mouseY);
+        xShift=x0Mem+(width/2-mouseX);
+        yShift=y0Mem+(height/2-mouseY);
+        x0 = xShift;
+        y0 = yShift;
 
 
     }
@@ -83,8 +108,8 @@ public class VisualDraw {
 
     private static int getColor(Complex z0, int paletteSize) {
         Complex z = z0;
-
-        for (int i = paletteSize - 1; i > 1; i--) {
+        int hole=1;
+        for (int i = paletteSize - 1; i > 1; i-=hole) {
             if (z.abs() > 2) {
                 return i;
             }
